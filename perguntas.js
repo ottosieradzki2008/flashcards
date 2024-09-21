7ac045e3d1460c61fa20d5794e3b2340c2684420
Otto <132456405+ottosieradzki2008@users.noreply.github.com>
2024-09-20 21:33:43 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é C#?',
+    'O C# é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual o país do Leste Europeu no qual as bandeiras tem a cor azul, branco e preto?',
+    'o país é a Estônia'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz atualmente em Inglês?',
+    'Atualmente em ingles é Currently (Cârentli)'
+)
